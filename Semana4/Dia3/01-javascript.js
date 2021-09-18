eddf482a82fde8661ed359f1d92655501aa5a1ca
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-18 18:02:57 -0500

    update

---
+++ Semana4/Dia3/01-javascript.js
@@ -0,0 +1,31 @@
+// console.log permite mostrar en la consola de JS, lo que se nos antoje es muy útil para saber que esta pasando internamente dentro de JS
+console.log("Hola Mundo!");
+
+// Variables
+// Palabras reservadas, JS las usa
+//los nombres de variables son sensibles a Mayus y Minus
+var nombre = "Jorge";
+
+console.log(nombre); //Solamente para mostrar algo
+
+//Tipos de gatos -Texto, Números, Booleanos,
+
+//String - Texto - Cadena de Texto
+var nombre2 = "Osmar";
+
+//Number - número
+var edad = 31;
+
+console.log(edad);
+
+//Boolean - Booleano
+var esDeNoche = true; //true ó false
+console.log(esDeNoche);
+
+// para ver el tipo de dato
+console.log(typeof esDeNoche);
+
+//para cambiar de valor a una variable, me basta con llamarla, esta variable tiene que existir antes
+edad = 25;
+
+console.log(edad);
