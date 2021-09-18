48141f9a4d3a31b7a73bd8cf24693dc09179f0b2
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-18 18:59:22 -0500

    update

--- Semana4/Dia3/01-javascript.js
+++ Semana4/Dia3/01-javascript.js
@@ -29,3 +29,41 @@ console.log(typeof esDeNoche);
 edad = 25;
 
 console.log(edad);
+
+nombre = "Eduardo";
+
+console.log(nombre);
+
+//Cambiar de tipo de dato
+
+var anio = 2021;
+
+anio = "dos mil veintidos";
+
+console.log(anio);
+
+//JS es un lenguage que no es tipado, no pre declaramos los tipos de datos
+
+//VALORES RAROS EN VARIABLES
+
+var nota; //undefined, no esta definido
+
+console.log(nota);
+
+var nulo = null; //deberia haber algo, pero no esta
+
+console.log(nulo);
+
+// Not a Number = NaN
+var rareza = 100 / "cebollas";
+
+console.log(rareza);
+
+//Infinito
+var infinito = 100 / 0;
+
+infinito = null;
+
+console.log(infinito);
+
+console.log(typeof infinito);
---
+++ Semana4/Dia3/02-aritmetica.js
@@ -0,0 +1,22 @@
+// desde ahora utilizemos let
+let a = 100;
+
+let b = 4;
+
+let suma = a + b;
+console.log("Esto es una suma", suma);
+
+// puedo poner directamente código de JS dentro del console.log(para ver su resultado)
+console.log(1000 + 50);
+
+let resta = a - b;
+console.log("Resta", resta);
+
+let multiplicacion = a * b;
+console.log("Multiplicación", multiplicacion);
+
+let division = a / b;
+console.log("División", division);
+
+let residuo = 105 % 4;
+console.log("residuo o módulo", residuo);
