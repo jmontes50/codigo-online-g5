716f7196139315f33ece042bfd1faf73fc576052
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-23 18:38:55 -0500

    update

---
+++ Semana5/Dia1/01-concatenacion.js
@@ -0,0 +1,28 @@
+let a = 1;
+
+let b = "4"; //string
+
+let c = a + b;
+
+console.log(typeof c);
+
+let nombre = "Jorge";
+
+let apellido = "Montesinos";
+
+let nombreCompleto = nombre + " " + apellido;
+
+console.log(nombreCompleto);
+
+//TEMPLATE STRING / TEMPLATE LITERAL
+//Backticks ``
+// ${acá adentro, podemos poner código de JS}
+
+let edad = 31;
+
+let texto = `Hola soy ${nombre} ${apellido} 
+y soy de Aqp, tengo ${edad} años`;
+
+console.log(texto);
+
+//PROMPT, capturar datos, del usuario
