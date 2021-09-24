21b9b333457b68ac9503476a503ed07554f024b1
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-24 18:50:29 -0500

    update

---
+++ Semana5/Dia2/01-switch.js
@@ -0,0 +1,41 @@
+// let producto = +prompt("Ingrese el código del producto");
+
+// switch (producto) {
+//     //case posible_valor, este puede ser un num, booleano, string
+//     case 1:
+//         console.log("Chocolate");
+//         //todo el código que quiera
+//         break; //corta la ejecución, como un return
+//     case 2:
+//         console.log("Papitas");
+//         break;
+//     case 3:
+//         console.log("Galletas");
+//         break;
+//     case 4:
+//         console.log("Pollo");
+//         break;
+//     default:
+//         //else
+//         console.log("Código no encontrado");
+// }
+
+let horas = +prompt("Ingrese sus horas semanales");
+
+//si el trabajador trabaja mas de 35 horas tiene horas extra
+
+//si el trabajador tiene igual o mas de 30 hasta menos de 35 horas, cumplio con su jornada
+
+//si tiene menos de 30 se le debe mandar un aviso
+
+// horas > 35 -----> horas extra
+// horas >= 30 && horas <= 35 ----> cumplio
+
+switch (true) {
+    case horas > 35:
+        console.log("horas extra!");
+        break;
+    case horas >= 30 && horas <= 35:
+        console.log("Cumplio su jornada");
+        break;
+}
---
+++ Semana5/Dia2/02-ternario.js
@@ -0,0 +1,11 @@
+let edad = 19;
+
+// if (edad >= 18) {
+//     console.log("dni azul");
+// } else {
+//     console.log("dni amarillo");
+// }
+
+//OPERADOR TERNARIO
+// condicional ? si es verdadero : si es falso
+edad >= 18 ? console.log("dni azul") : console.log("dni amarillo");
