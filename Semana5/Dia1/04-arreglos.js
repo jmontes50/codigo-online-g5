f6bbec81d9858d079325b93bcf346dbe80ca9d13
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-23 21:14:26 -0500

    update

--- Semana5/Dia1/04-arreglos.js
+++ Semana5/Dia1/04-arreglos.js
@@ -1,7 +1,24 @@
-let nombres = ["Masami", "Oscar", "Mauro", "Melissa", "Frank"];
+//para declarar arreglos, necesitamos ponerlos entre []
+//desde el primer elemento se cuenta desde 0, 1 ,2 ...
+//ese número se conoce como posición, índice o index
+let nombres = ["Masami", "Oscar", "Mauro", "Melissa", "Frank", "Julio"];
 
+//si deseo referenciar un elemento necesito su posicion
+//nombre_arreglo[#]
 console.log(nombres[4]);
 
 console.log(`Los nombres de mis alumn@s son ${nombres[0]}, ${nombres[1]}, ${nombres[2]}`);
 
 console.log(nombres);
+
+//longitud es la cantidad de elementos
+console.log(nombres.length); //number
+
+let cofre = ["gato", 100, true];
+
+console.log(cofre);
+
+//agregar items, al final de un arreglo
+cofre.push("perrete");
+
+console.log("cofre actualizado", cofre);
