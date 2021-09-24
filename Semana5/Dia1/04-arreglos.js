10708667802f0fcfa4324f39806d17c34a66a61c
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-23 21:01:09 -0500

    update

---
+++ Semana5/Dia1/04-arreglos.js
@@ -0,0 +1,7 @@
+let nombres = ["Masami", "Oscar", "Mauro", "Melissa", "Frank"];
+
+console.log(nombres[4]);
+
+console.log(`Los nombres de mis alumn@s son ${nombres[0]}, ${nombres[1]}, ${nombres[2]}`);
+
+console.log(nombres);
