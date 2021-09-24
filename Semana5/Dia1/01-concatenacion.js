74b79242c5cc32f476f6e059613a1579238c11e0
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-23 19:02:33 -0500

    update

--- Semana5/Dia1/01-concatenacion.js
+++ Semana5/Dia1/01-concatenacion.js
@@ -26,3 +26,17 @@ y soy de Aqp, tengo ${edad} años`;
 console.log(texto);
 
 //PROMPT, capturar datos, del usuario
+let cantidadMascotas = prompt(`Hola ${nombre}, cuantas mascotas tienes?`);
+
+console.log(`Mascotas : ${cantidadMascotas}`);
+
+console.log(typeof cantidadMascotas); //todo lo ingresado en un prompt es string
+
+let convertido = +cantidadMascotas;
+//parseInt,parseFloat, parse = interpretar, convierte de texto a número
+//int - solo enteros
+//float - acepta decimales
+// let convertido = parseInt(cantidadMascotas);
+// let convertido = parseFloat(cantidadMascotas);
+
+console.log(typeof convertido, convertido, "PATATA");
