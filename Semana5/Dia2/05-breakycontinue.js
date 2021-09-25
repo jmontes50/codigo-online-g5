a19d90bbb3c03c8a38d5e6764fbabbbef60c870d
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-24 20:38:46 -0500

    update

--- Semana5/Dia2/05-breakycontinue.js
+++ Semana5/Dia2/05-breakycontinue.js
@@ -1,3 +1,4 @@
+/*
 let alumnos = ["Leonardo", "Arturo", "Gustavo", "Ariana", "Alan", "Masami"];
 
 //algortimo para que el usuario ingrese un nombre y busque si existe el alumno y me muestre la posicion de ese alumno
@@ -12,4 +13,17 @@ for (let i = 0; i < alumnos.length; i++) {
         break;
     }
     console.log(`Buscando... en ${i}`);
+}
+*/
+
+let notas = [17, 15, 10, 5, 14, 20, 7, 9, 18, 1, 0, 13];
+
+//saber las notas aprobatorias
+
+for (let i = 0; i < notas.length; i++) {
+    if (notas[i] < 12) {
+        continue;
+    }
+
+    console.log(notas[i]);
 }
