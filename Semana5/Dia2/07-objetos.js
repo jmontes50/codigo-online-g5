86458b8368f9e34532f3d4d79e42eade3db99d64
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-24 22:05:28 -0500

    update

--- Semana5/Dia2/07-objetos.js
+++ Semana5/Dia2/07-objetos.js
@@ -24,4 +24,18 @@ let albumMusical = {
     nombre: "After Hours",
     fechaLanzamiento: "20/03/2020",
     genero: ["Dream Pop", "Drum and Bass"],
+    canciones: [
+        {
+            titulo: "Heartless",
+            duracion: 3.21,
+            autor: ["Abel Tesfaye", "Leland Wayne"],
+        },
+        {
+            titulo: "Blinding Lights",
+            duracion: 3.22,
+            autor: ["Abel Tesyafe", "Ahmad Balshe", "Jason Quennevile"],
+        },
+    ],
 };
+
+console.log(albumMusical.canciones[1].autor[2]);
