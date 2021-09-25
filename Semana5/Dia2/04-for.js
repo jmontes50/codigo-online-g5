cbc19e2f6c63f0020473f46ee1f5ad5af0af160a
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-24 19:31:48 -0500

    update

---
+++ Semana5/Dia2/04-for.js
@@ -0,0 +1,20 @@
+/**
+ * for(contador; condicional; actualización_contador){
+ * 	   codigo que se va a repetir
+ * }
+ */
+//i++
+//i = i + 1;
+
+for (let i = 0; i <= 1; i = i + 1) {
+    console.log(`El contador es :${i}`);
+}
+
+console.log("yo estoy al final!");
+
+let ropa = ["Bufanda", "Camisa", "Guantes", "Jeans"];
+
+//length = longitud, es la cant items en un arreglo
+for (let i = 0; i < ropa.length; i++) {
+    console.log(ropa[i]);
+}
