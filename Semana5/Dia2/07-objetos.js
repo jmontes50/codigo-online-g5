c1ee7e3fcdf631d0b5460b98a9b7872b33dcd6cb
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-24 21:56:54 -0500

    update

---
+++ Semana5/Dia2/07-objetos.js
@@ -0,0 +1,27 @@
+let mascota = {
+    nombre: "Backus",
+    raza: "Poodle",
+    color: "blanco",
+    edad: 13,
+    hobbies: ["Jugar", "Perseguir gatos", "Comer pate"],
+    comer: function () {
+        console.log("ñam ñam ñam");
+    },
+    ladra: true,
+};
+
+//objeto.propiedad
+
+// console.log(mascota.nombre);
+
+// console.log(mascota.edad);
+
+// console.log(mascota.hobbies[0]);
+
+// mascota.comer();
+
+let albumMusical = {
+    nombre: "After Hours",
+    fechaLanzamiento: "20/03/2020",
+    genero: ["Dream Pop", "Drum and Bass"],
+};
