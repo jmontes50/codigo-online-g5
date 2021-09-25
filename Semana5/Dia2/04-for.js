41e07264051fa69ba8401b9e0492eff01b4cf8a3
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-24 19:59:57 -0500

    update

--- Semana5/Dia2/04-for.js
+++ Semana5/Dia2/04-for.js
@@ -6,6 +6,7 @@
 //i++
 //i = i + 1;
 
+/*
 for (let i = 0; i <= 1; i = i + 1) {
     console.log(`El contador es :${i}`);
 }
@@ -17,4 +18,15 @@ let ropa = ["Bufanda", "Camisa", "Guantes", "Jeans"];
 //length = longitud, es la cant items en un arreglo
 for (let i = 0; i < ropa.length; i++) {
     console.log(ropa[i]);
+}
+*/
+
+let carta = ["Ceviche", "Pollo a la brasa", "Costillar", "Adobo", "Caldo de gallina"];
+
+let eleccion = +prompt("0. Ceviche, 1. Pollo, 2.Costillar, 3.Adobo, 4. Caldo de Gallina");
+
+for (let i = 0; i < carta.length; i++) {
+    if (eleccion == i) {
+        console.log(`Ud. a elegido: ${carta[i]}`);
+    }
 }
