cda4605d40922c5d6b333c4821f02338d9ccba80
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-30 18:50:30 -0500

    update

---
+++ Semana5/Dia3/05-metodosenarreglos.js
@@ -0,0 +1,13 @@
+let bebidas = ["Agua", "Limonada", "Mojito", "Pisco Sour"];
+
+//elimina el último elemento de un arreglo
+// bebidas.pop();
+
+console.table(bebidas);
+
+//foreach
+
+bebidas.forEach(function (miBebida) {
+    //dentro de este forEach, me permite hacer por cada item lo que yo desee
+    console.log(miBebida);
+});
---
+++ Semana6/Dia1/01-objetosNaturales.js
@@ -0,0 +1,30 @@
+//objeto Date nos sirve para manejar fechas
+let hoy = new Date();
+//clases (class), plano, diseño, molde
+//cuando invocamos una clase utilizamos la palabra new
+
+console.log(hoy);
+console.log(typeof hoy);
+
+//Obtengo el año
+console.log(hoy.getFullYear());
+
+//Mes, Enero es 0 y Diciembre es 11
+console.log(hoy.getMonth());
+
+//Día
+console.log(hoy.getDate());
+
+let navidad = new Date(2021, 11, 25);
+
+console.log(navidad);
+//getTime() me devuelve el timestamp de navidad
+console.log(navidad.getTime());
+
+let hastaNavidad = navidad - hoy;
+
+console.log(hastaNavidad);
+
+let diasParaNavidad = hastaNavidad / 1000 / 60 / 60 / 24;
+
+console.log(diasParaNavidad.toFixed(2));
