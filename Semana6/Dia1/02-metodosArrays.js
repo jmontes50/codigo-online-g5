d3950b689bc880ba3d91a89a592104e3ee998e0c
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-30 20:19:02 -0500

    update

--- Semana6/Dia1/02-metodosArrays.js
+++ Semana6/Dia1/02-metodosArrays.js
@@ -11,3 +11,24 @@ frutas.forEach(function (fru) {
 });
 
 //forEach por cada elemento que encuentre vas a ejecutar una función, y cada vez que se ejecute esa funcion vas a recibir c/item en orden y este item lo podemos utilizar dentro de la función
+
+let energia = [89, 90, 86, 85, 100];
+
+//energía se va a dividir en 2
+//Como podría tener un nuevo arreglo con los valores que representen la mitad de la energía por cada item
+
+// let energiaCima = [];
+// for (let i = 0; i < energia.length; i++) {
+//     energiaCima.push(energia[i] / 2);
+// }
+// console.log(energiaCima);
+
+//crea un nuevo array pero con informacion transformada
+let energiaCima = energia.map(function (energy) {
+    // let energiaTransformada = energy / 2;
+    // return energiaTransformada;
+    return energy / 2;
+});
+
+console.log(energiaCima);
+console.log(energia);
