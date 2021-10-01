38d3248ea6401c82723731085c5257a4d9403651
Jorge Montesinos <osmarmontes50@gmail.com>
2021-09-30 22:00:37 -0500

    update

--- Semana6/Dia1/03-objetosDOM.js
+++ Semana6/Dia1/03-objetosDOM.js
@@ -9,11 +9,30 @@ console.log(`URL: ${window.location}`);
 navigator.geolocation.getCurrentPosition(function (ubicacion) {
     console.log(ubicacion);
 });
-//Javascript a travez del objeto window
+//Javascript a traves del objeto window
 //window = pestaña del navegador
 
 console.log(window.document);
 
+//estoy capturando un elemento
 let miTitulo = document.querySelector("h1");
 
 console.log(miTitulo);
+
+//innerHTML, es el contenido HTML de un elemento
+miTitulo.innerHTML = "Hola soy un nuevo Titulo";
+
+//EL HTML DENTRO DE LA ETIQUETA
+console.log(miTitulo.innerHTML);
+
+//obtener un elemento
+let miCaja = document.getElementById("caja");
+
+console.log(miCaja);
+
+//innerHTML, representa el contenido HTML de un elemento
+miCaja.innerHTML = "<h2>Titulo Caja</h2>";
+
+//background-color = backgroundColor
+miCaja.style.backgroundColor = "purple";
+miCaja.style.color = "white";
