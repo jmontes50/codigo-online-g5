04feb8e16814de47172fd99c0164588e14d83931
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-01 21:32:42 -0500

    update

--- Semana6/Dia2/02-eventos.js
+++ Semana6/Dia2/02-eventos.js
@@ -5,3 +5,34 @@ let miBoton = document.createElement("button");
 miBoton.innerText = "Clickeame!";
 
 divContenido.appendChild(miBoton);
+
+//elementoHTML,addEventListener("evento",function(){})
+miBoton.addEventListener("click", function () {
+    alert("Haz hecho click!");
+});
+
+let peliculas = [
+    "Parasite",
+    "Shang Chi",
+    "Interstellar",
+    "Matrix",
+    "El bueno, el malo y el feo",
+    "Geo tormenta",
+    "Jhon Wick",
+    "Constantine",
+    "Godfather",
+    "El Señor de los anillos",
+];
+
+peliculas.forEach(function (peli) {
+    let nuevoParrafo = document.createElement("p");
+    nuevoParrafo.innerHTML = peli;
+    divContenido.appendChild(nuevoParrafo);
+
+    nuevoParrafo.style.cursor = "pointer";
+
+    nuevoParrafo.addEventListener("dblclick", function () {
+        alert(`Hiciste click en ${peli}`);
+        nuevoParrafo.style.backgroundColor = "steelblue";
+    });
+});
