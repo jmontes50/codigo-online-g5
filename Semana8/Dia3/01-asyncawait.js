751689e78a5b03bdd3a9a04e43c4bbd079ca768a
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-16 15:50:37 -0500

    update

--- Semana8/Dia3/01-asyncawait.js
+++ Semana8/Dia3/01-asyncawait.js
@@ -1,11 +1,35 @@
 //de Promesas a ASYNC
-const traerData = () => {
-    return new Promise((resolve, reject) => {
-        resolve("Data Obtenida!!!");
-        // reject("Error al obtener Data")
-    });
+// const traerData = () => {
+//     return new Promise((resolve, reject) => {
+//         resolve("Data Obtenida!!!");
+//         // reject("Error al obtener Data")
+//     });
+// };
+
+//async, me devuelve una promesa en vez de retornarme el resultado directamente
+const traerData = async () => {
+    //acá adentro yo tenga más código, haga operaciones un if, etc
+    //y cuando tenga exito return si fallo throw
+    // return "Data Obtenida con async"; //return = resolve
+    throw "Error al obtener data con async"; //throw = reject
 };
 
-traerData()
-    .then((rpta) => console.log(rpta))
-    .catch((error) => console.log(error));
+// traerData()
+//     .then((rpta) => console.log(rpta))
+//     .catch((error) => console.log(error));
+
+const usarData = async () => {
+    //cuandoi utilicemos await, tiene que estar dentro de una función
+    //y esa función tiene que tener async
+    //trycatch no es solamente para async/await
+    try {
+        //intenta esto
+        let rpta = await traerData();
+        console.log(rpta);
+    } catch (error) {
+        //y si no te funciona ejecuta esto y me dices que ha pasado
+        //va a capturar el reject o throw de la función
+        console.log(error);
+    }
+};
+usarData();
