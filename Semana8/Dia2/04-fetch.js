6b5b234fa83c3ddcfc6fa1fe5c944fb078cf79e2
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-15 21:57:09 -0500

    update

--- Semana8/Dia2/04-fetch.js
+++ Semana8/Dia2/04-fetch.js
@@ -1,8 +1,25 @@
+//GET
 fetch("https://reqres.in/api/users?page=2")
     .then((respuesta) => {
-        console.log(respuesta);
+        // console.log(respuesta);
         return respuesta.json(); //parsea de texto a JS
     })
     .then((gatos) => {
-        console.log(gatos);
+        // console.table(gatos.data);
     });
+
+//POST - Crear
+let nuevoUsuario = {
+    name: "Daniel de la Cruz",
+    job: "BOSS",
+};
+
+//headers - cabeceras - guía de remisión
+let cabecera = {
+    method: "POST", //Método HTTP, POST, PUT, DELETE, GET
+    headers: {
+        //Que info envio, de que tipo, para que el backend sepa como procesarlo
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify(nuevoUsuario), //data,carga, pero hay que transformarlo a JSON
+};
