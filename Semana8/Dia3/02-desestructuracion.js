d8e52f2eb85a0cd8fe2fbb5d5b2776c5bf25bf69
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-16 16:29:58 -0500

    update

---
+++ Semana8/Dia3/02-desestructuracion.js
@@ -0,0 +1,27 @@
+//desestructuracion en arreglos
+let frutas = ["Piña", "fresa", "naranja", "kiwi"];
+
+// console.log(frutas[2]);
+// console.log(frutas[3]);
+
+let fruta1 = frutas[0];
+let fruta2 = frutas[1];
+
+//En una sola linea para cada item
+let [fru1, fru2, fru3, fru4] = frutas;
+
+// console.log(fru4);
+// console.log(fru2);
+// console.log(fru1);
+
+//desestructuracion de objetos
+let celular = {
+    pantalla: 6.9,
+    color: "negro",
+    memoria: 4,
+    camara: 12,
+    marca: "Motorola",
+    encender: function () {
+        console.log("tururu");
+    },
+};
