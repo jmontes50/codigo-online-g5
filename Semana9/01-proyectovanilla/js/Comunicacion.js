c1449e24a130e2626dabe4f66b28e0a241a7d776
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-28 18:59:23 -0500

    update

---
+++ Semana9/01-proyectovanilla/js/Comunicacion.js
@@ -0,0 +1,17 @@
+// se va encargar de hacer peticiones
+
+const URL = "https://616b5ead16c3fa001717167c.mockapi.io/productos";
+
+const obtenerProductos = async () => {
+    try {
+        //intenta esto
+        //await , le deciamos aguanta, esto se tiene que resolver
+        //respuesta del fetch, me da el status, 200, cabeceras del response, NO ME DA UN JSON
+        const respuesta = await fetch(URL); //solamente la URL, es un GET, respuesta de la petición
+        const productos = await respuesta.json(); //aquí hay otra promesa y esto me da el JSON convertido
+        return productos; //resolve(productos)
+    } catch (error) {
+        //y si falla me pasas la voz
+        throw error;
+    }
+};
