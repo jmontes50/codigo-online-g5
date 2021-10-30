cf0da9b9e99474cbd725c74dc43e50009479a42d
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-29 21:45:01 -0500

    update

---
+++ Semana9/introreact/hola-react/src/components/Main.js
@@ -0,0 +1,12 @@
+//Reglas del componente
+//.1 función o clase
+//.2 el nombre de la función/componente tiene que comenzar en Mayúsculas
+//.3 esa función/componente tiene que retornar algo
+//.3.1 el return sera el JSX(HTML) que se dibujará
+//.4 tiene que exportarse por defecto, para que otros componentes, lo puedan reutilizar
+
+function Main() {
+    return <main></main>;
+}
+
+export default Main;
