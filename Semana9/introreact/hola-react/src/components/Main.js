a89ecbe7e319002fce13df0483d7deaedf9ff978
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-30 16:31:18 -0500

    update

--- Semana9/introreact/hola-react/src/components/Main.js
+++ Semana9/introreact/hola-react/src/components/Main.js
@@ -9,12 +9,14 @@
 //los props se reciben en el hijo con el nombre del "atributo" que se le puso en el componente Padre
 //estos props son como los parámetros de una función
 //<Main subtitulo="ewrw" tituloPadre={estado} actualizarInfo={setEstado} />
-function Main({subtitulo, tituloPadre, actualizarTitulo}) {
+function Main({subtitulo, tituloPadre, actualizarTitulo, mostrarInfo}) {
 
     return (<main>
 		<div>
 			<h2>{subtitulo}</h2>
-			<p>Lorem ipsum dolor sit amet.</p>
+			{/* renderizado condicional, a partir props, estado, variable*/}
+			{/* si mostrarInfo es true ? muestro esto si es verdad : muestro esto otro si es false */}
+			{mostrarInfo ? (<p>Lorem ipsum dolor sit amet.</p>) : null}
 			<p>El titulo de App es {tituloPadre}</p>
 			<button onClick={() => {actualizarTitulo("Titulo desde Main")}}>
 				Actualizar desde Main
