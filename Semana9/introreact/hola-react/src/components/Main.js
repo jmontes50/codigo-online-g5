b98ce03afb4941058001d086d9312333f87ec209
Jorge Montesinos <osmarmontes50@gmail.com>
2021-10-29 21:49:10 -0500

    update

--- Semana9/introreact/hola-react/src/components/Main.js
+++ Semana9/introreact/hola-react/src/components/Main.js
@@ -3,10 +3,16 @@
 //.2 el nombre de la función/componente tiene que comenzar en Mayúsculas
 //.3 esa función/componente tiene que retornar algo
 //.3.1 el return sera el JSX(HTML) que se dibujará
+//.3.2 JSX utiliza (<html></html>) para indicar que dibujará elementos
 //.4 tiene que exportarse por defecto, para que otros componentes, lo puedan reutilizar
 
 function Main() {
-    return <main></main>;
+    return (<main>
+		<div>
+			<h2>Subtitulo</h2>
+			<p>Lorem ipsum dolor sit amet.</p>
+		</div>
+	</main>);
 }
 
 export default Main;
