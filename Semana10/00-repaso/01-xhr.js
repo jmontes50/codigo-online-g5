e664135fa9d206f9b83bd89db3a8e09ebd6d54b2
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-03 18:48:09 -0500

    UPDATE

--- Semana10/00-repaso/01-xhr.js
+++ Semana10/00-repaso/01-xhr.js
@@ -1,22 +1,30 @@
 let hacerPeticion = (tareaFinal) => {
     let asistente = new XMLHttpRequest();
 
+    //solo estamos escuchando al evento, no estamos ejecutando nada
+    //asistente instrucciones de lo que tiene que hacer
     asistente.addEventListener("readystatechange", () => {
         switch (asistente.readyState) {
             case 1:
-                console.log("open", asistente.readyState);
+                console.log("open", asistente.readyState); //pon el destinatario y dirección
                 break;
             case 2:
-                console.log("send", asistente.readyState);
+                console.log("send", asistente.readyState); //envialo, por el buzón
                 break;
             case 3:
-                console.log("recepción", asistente.readyState);
+                console.log("recepción", asistente.readyState); //avisas si llego al destinataro o a serpost
                 break;
             case 4:
-                console.log("respuesta del server", asistente.readyState);
+                console.log("respuesta del server", asistente.readyState); //me avisas cuando el dest. responda
                 break;
             default:
                 console.log("algo paso");
         }
     });
+
+    //xomo si mandará una carta indico los datos y la envio
+    asistente.open("GET", "https://swapi.dev/api/people/1"); //pon el dest. y la forma de envio
+    asistente.send(); //envia la carta
 };
+
+hacerPeticion();
