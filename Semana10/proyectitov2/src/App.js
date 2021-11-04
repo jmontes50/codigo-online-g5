e3eed9e54b20e52d3aad457e4a60f76901087e22
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-03 20:28:58 -0500

    update

---
+++ Semana10/proyectitov2/src/App.js
@@ -0,0 +1,11 @@
+//Uds pueden importar, imagenes, hojas de estilo, pero si no es JS necesito ponerle la .extension
+import "./css/estilos.css";
+
+export default function App() {
+    return (
+        <div className="contenedor">
+            <div className="aplicacion"></div>
+            <div className="boleta"></div>
+        </div>
+    );
+}
