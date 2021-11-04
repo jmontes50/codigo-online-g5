325c0acbe64c4116fa15ccfe86304aaa00937682
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-03 20:38:06 -0500

    update

--- Semana10/proyectitov2/src/App.js
+++ Semana10/proyectitov2/src/App.js
@@ -1,4 +1,6 @@
 //Uds pueden importar, imagenes, hojas de estilo, pero si no es JS necesito ponerle la .extension
+//OJO: los estilos que yo importo no afectan solamente al componente en el que lo estoy importando
+//afectan a toda la aplicacion
 import "./css/estilos.css";
 
 export default function App() {
