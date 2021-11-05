9f68f946172b2ca6caaf8b5049d8a8e094fd4fb7
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-04 21:17:33 -0500

    update

--- Semana10/administrador/src/components/FormProducto.js
+++ Semana10/administrador/src/components/FormProducto.js
@@ -5,17 +5,17 @@ export default function FormProducto({ value }) {
             <form>
                 <div className="mb-3">
                     <label className="form-label">Nombre producto</label>
-                    <input type="text" className="form-control" name="nombre" />
+                    <input type="text" className="form-control" name="nombre" value={value.nombre} />
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">Descripción producto</label>
-                    <input type="text" className="form-control" name="descripcion" />
+                    <input type="text" className="form-control" name="descripcion" value={value.descripcion} />
                 </div>
 
                 <div className="mb-3">
                     <label className="form-label">Precio producto</label>
-                    <input type="number" className="form-control" name="precio" />
+                    <input type="number" className="form-control" name="precio" value={value.precio} />
                 </div>
             </form>
         </div>
