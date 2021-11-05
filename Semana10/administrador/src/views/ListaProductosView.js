ff80d217062433d1a920c08f93ddf8eedc225498
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-04 20:30:14 -0500

    update

--- Semana10/administrador/src/views/ListaProductosView.js
+++ Semana10/administrador/src/views/ListaProductosView.js
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { obtenerProductos } from "../services/productosService";
 
 export default function ListaProductosView() {
+    const [productos, setProductos] = useState([]);
+
     const getProductos = async () => {
         try {
             const productosObtenidos = await obtenerProductos();
-            console.table(productosObtenidos);
+            // console.table(productosObtenidos);
+            setProductos(productosObtenidos); //después que obtenemos los datos, actualizamos el estado
         } catch (error) {
             console.log(error);
         }
@@ -15,5 +18,29 @@ export default function ListaProductosView() {
         getProductos();
     }, []);
 
-    return <div></div>;
+    return (
+        <div>
+            <h1>Productos registrados</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>Precio</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {productos.map(({ nombre, descripcion, precio }, i) => (
+                        <tr key={i}>
+                            <td>{nombre}</td>
+                            <td>{descripcion}</td>
+                            <td>{precio}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
