bd25a99f8ec86714ca065d3c69aac9995bb78c74
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-04 22:09:00 -0500

    update

--- Semana10/administrador/src/services/productosService.js
+++ Semana10/administrador/src/services/productosService.js
@@ -2,7 +2,8 @@ import axios from "axios"; //fetch pero mejor
 
 //conjunto de funciones para hacer peticiones
 
-const URL = "https://616b5ead16c3fa001717167c.mockapi.io/productos";
+//https://616b5ead16c3fa001717167c.mockapi.io/productos
+const URL = `${process.env.REACT_APP_API}productos`;
 
 const obtenerProductos = async () => {
     try {
