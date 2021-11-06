d0bb0b7251b94e5b50b6f425090b66f43af43640
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-05 21:07:42 -0500

    update

--- Semana10/administrador/src/views/EditarProductoView.js
+++ Semana10/administrador/src/views/EditarProductoView.js
@@ -2,7 +2,7 @@ import FormProducto from "../components/FormProducto";
 import { useParams } from "react-router-dom";
 
 export default function EditarProductoView() {
-    const params = useParams();
+    const { id } = useParams();
 
     return <div></div>;
 }
