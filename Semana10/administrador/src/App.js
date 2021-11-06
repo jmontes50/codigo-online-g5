16650ca294e9adb9b353231ccfab10e96f20a5b1
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-05 19:32:34 -0500

    update

--- Semana10/administrador/src/App.js
+++ Semana10/administrador/src/App.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css"; //node_modules
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ListaProductosView from "./views/ListaProductosView";
+import CrearProductoView from "./views/CrearProductoView";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
                 <Routes>
                     {/* Route path="/url" element={<Componente />}*/}
                     <Route path="/" element={<ListaProductosView />} />
+                    <Route path="/crearproducto" element={<CrearProductoView />} />
                 </Routes>
             </Router>
         </div>
