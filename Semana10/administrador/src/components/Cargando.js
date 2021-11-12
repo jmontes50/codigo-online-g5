3f2d2758938b3436d0180cc6f5da797b57451b90
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-11 22:05:30 -0500

    update

--- Semana10/administrador/src/components/Cargando.js
+++ Semana10/administrador/src/components/Cargando.js
@@ -1,7 +1,26 @@
 export default function Cargando() {
     return (
-        <div>
-            <i className="fas fa-cog fa-spin fa-6x"></i>
+        // los estilos inline en React son un objeto
+        <div
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+                zIndex: 99,
+                backgroundColor: "rgba(255,255,255,0.5)",
+            }}
+        >
+            <i
+                className="fas fa-cog fa-spin fa-6x"
+                style={{
+                    color: "slateblue",
+                    position: "absolute",
+                    top: "calc(50% - 50px)",
+                    left: "calc(50% - 50px)",
+                }}
+            ></i>
         </div>
     );
 }
