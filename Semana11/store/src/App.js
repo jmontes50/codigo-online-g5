c033a34c282879bfd7b60f8987fe98297a5d3d37
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-13 17:00:21 -0500

    update

--- Semana11/store/src/App.js
+++ Semana11/store/src/App.js
@@ -1,5 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { AuthContextProvider } from "./context/authContext";
+import LoginView from "./views/LoginView";
+
 export default function App() {
-    return <div></div>;
+    return (
+        <AuthContextProvider>
+            {/* aquí dentro del provider puedo poner tanto componentes como rutas */}
+            <LoginView />
+        </AuthContextProvider>
+    );
 }
