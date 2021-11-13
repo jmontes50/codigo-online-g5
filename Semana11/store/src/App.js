d23ee1f66270b25cc4b123d5a69bd67833e40c71
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-13 18:18:07 -0500

    update

--- Semana11/store/src/App.js
+++ Semana11/store/src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeView from "./views/HomeView";
 import LoginView from "./views/LoginView";
 import CheckOutView from "./views/CheckOutView";
+import Navegacion from "./components/Navegacion";
 
 export default function App() {
     return (
         <AuthContextProvider>
             {/* aquí dentro del provider puedo poner tanto componentes como rutas */}
             <Router>
+                <Navegacion />
                 <Routes>
                     <Route path="/" element={<HomeView />} />
                     <Route path="/login" element={<LoginView />} />
---
+++ Semana11/store/src/components/Navegacion.js
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
+import { Navbar, Container, Nav, NavLink } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function Navegacion() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand>Store 5</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link className="nav-link" to="/checkout">
+                            Checkout
+                        </Link>
+                    </Nav>
+                    <Nav>
+                        <NavLink>Salir</NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
