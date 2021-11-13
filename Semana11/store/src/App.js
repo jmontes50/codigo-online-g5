e5a20b3b15674ec4e208ced77277060a17fdfc07
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-13 18:00:08 -0500

    update

--- Semana11/store/src/App.js
+++ Semana11/store/src/App.js
@@ -1,13 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import { AuthContextProvider } from "./context/authContext";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeView from "./views/HomeView";
 import LoginView from "./views/LoginView";
+import CheckOutView from "./views/CheckOutView";
 
 export default function App() {
     return (
         <AuthContextProvider>
             {/* aquí dentro del provider puedo poner tanto componentes como rutas */}
-            <LoginView />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomeView />} />
+                    <Route path="/login" element={<LoginView />} />
+                    <Route path="/checkout" element={<CheckOutView />} />
+                </Routes>
+            </Router>
         </AuthContextProvider>
     );
 }
