79ee5064d13524a3de085eb941a05cba6cca7d3e
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-20 16:07:35 -0500

    update

---
+++ Semana11/store/src/views/ProductoConFiltrosView.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { obtenerProductosPorPagina } from "../services/productoService";
+import ProductoCard from "../components/ProductoCard";
+
+export default function ProductoConFiltrosView() {
+    const [productos, setProductos] = useState([]);
+    const [pagina, setPagina] = useState(1);
+    const [limite, setLimite] = useState(9);
+
+    const getData = async () => {
+        try {
+            const prodObtenidos = await obtenerProductosPorPagina(página, limite);
+            setProductos(prodObtenidos);
+        } catch (error) {
+            console.log(error); //Swal icon:"error"
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+    return (
+        <>
+            <div className="container">
+                <div class="row my-3">
+                    {productos.map((prod, i) => (
+                        <div className="col-12 col-md-6 col-lg-4" key={i}>
+                            <ProductoCard producto={prod} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
