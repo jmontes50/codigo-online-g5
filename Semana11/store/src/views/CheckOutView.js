6acedc59bea8ffba1244d4482c17ffb9c616d22b
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-23 18:46:26 -0500

    update

--- Semana11/store/src/views/CheckOutView.js
+++ Semana11/store/src/views/CheckOutView.js
@@ -1,3 +1,32 @@
+import { useContext } from "react";
+import { CarritoContext } from "../context/carritoContext";
+import { useForm } from "react-hook-form"; //useForm es un hook personalizado, para manejar formularios
+
 export default function CheckOutView() {
-    return <div>Yo soy el CheckOutView y soy Privado</div>;
+    const { carrito } = useContext(CarritoContext);
+
+    return (
+        <div className="container mt-4">
+            <h1>Checkout</h1>
+            <div className="row my-2">
+                <div className="col-12 col-md-6">
+                    <h5>Verifique el carrito</h5>
+                    <ul className="list-group">
+                        {carrito.map((prod, i) => (
+                            <li className="list-group-item d-flex justify-content-between" key={i}>
+                                <div>
+                                    <h6 className="fw-bold">{prod.nombre}</h6>
+                                    <small>Cantidad: {prod.cantidad}</small>
+                                </div>
+                                <div className="badge bg-dark rounded-pill p-4">
+                                    {/* toFixed(entero), me permite manejar decimales en un número */}
+                                    S/ {(prod.cantidad * prod.precio).toFixed(2)}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
