969b55546e846345c157bcad2c1552fd57c11443
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-24 18:55:26 -0500

    update

---
+++ Semana11/store/src/components/HeaderCarousel.js
@@ -0,0 +1,11 @@
+import { Carousel, CarouselItem } from "react-bootstrap";
+
+export default function HeaderCarousel() {
+    return (
+        <header>
+            <Carousel>
+                <Carousel.Item></Carousel.Item>
+            </Carousel>
+        </header>
+    );
+}
