771987609466d4f3f2e350f9a20a60e24e26426d
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-24 18:40:21 -0500

    update

--- Semana11/store/src/views/HomeView.js
+++ Semana11/store/src/views/HomeView.js
@@ -1,8 +1,12 @@
 import GrupoProdHome from "../components/GrupoProdHome";
+import GroupCategoria from "../components/GroupCategoria";
 
 export default function HomeView() {
     return (
         <div>
+            <h2 className="text-center my-4">Categorías</h2>
+            <GroupCategoria />
+            <h2 className="text-center my-4">Últimas novedades</h2>
             <GrupoProdHome />
         </div>
     );
