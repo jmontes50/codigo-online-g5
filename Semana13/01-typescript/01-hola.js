f3d63967b8c8c78dcc61bea86456a697a0339a4a
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-25 18:28:00 -0500

    update

--- Semana13/01-typescript/01-hola.js
+++ Semana13/01-typescript/01-hola.js
@@ -1,9 +1,14 @@
 var saludo = "Hola Mundo!";
 console.log(saludo);
 //Tipado
-var texto = "Chifa con wantan";
+var texto = "mil";
 var cantidad = 21;
 var teQuiere = true;
 var regalos = ["Flores", "Chocolates", "Peluches", "PS5"];
 var random = undefined; //básicamente cualquier cosa
 var random2 = { nombre: "Backus" }; //acepta cualquier cosa menos null o undefined
+//funciones
+var sumar = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(sumar(10, 200));
--- Semana13/01-typescript/01-hola.ts
+++ Semana13/01-typescript/01-hola.ts
@@ -4,7 +4,7 @@ console.log(saludo);
 
 //Tipado
 
-let texto:string = "Chifa con wantan";
+let texto:string = "mil";
 
 let cantidad:number = 21;
 
@@ -15,3 +15,11 @@ let regalos:Array<string> = ["Flores","Chocolates","Peluches","PS5"];
 let random:any = undefined //básicamente cualquier cosa
 
 let random2:unknown = {nombre:"Backus"} //acepta cualquier cosa menos null o undefined
+
+//funciones
+
+let sumar = (num1:number, num2:number):number => {
+	return num1 + num2
+};
+
+console.log(sumar(10, 200));
