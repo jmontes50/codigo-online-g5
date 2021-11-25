fb7ccce4fbc6d7584fba82c97d603b4eeba6f556
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-24 22:03:17 -0500

    update

---
+++ Semana13/01-typescript/01-hola.js
@@ -0,0 +1,9 @@
+var saludo = "Hola Mundo!";
+console.log(saludo);
+//Tipado
+var texto = "Chifa con wantan";
+var cantidad = 21;
+var teQuiere = true;
+var regalos = ["Flores", "Chocolates", "Peluches", "PS5"];
+var random = undefined; //básicamente cualquier cosa
+var random2 = { nombre: "Backus" }; //acepta cualquier cosa menos null o undefined
---
+++ Semana13/01-typescript/01-hola.ts
@@ -0,0 +1,17 @@
+let saludo = "Hola Mundo!";
+
+console.log(saludo);
+
+//Tipado
+
+let texto:string = "Chifa con wantan";
+
+let cantidad:number = 21;
+
+let teQuiere:boolean = true;
+
+let regalos:Array<string> = ["Flores","Chocolates","Peluches","PS5"];
+
+let random:any = undefined //básicamente cualquier cosa
+
+let random2:unknown = {nombre:"Backus"} //acepta cualquier cosa menos null o undefined
