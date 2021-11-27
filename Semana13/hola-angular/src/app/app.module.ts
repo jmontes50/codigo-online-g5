a5ef771b38293fdcce2314ace9bf54ed3fb6ba4a
Jorge Montesinos <osmarmontes50@gmail.com>
2021-11-26 19:50:38 -0500

    update

--- Semana13/hola-angular/src/app/app.module.ts
+++ Semana13/hola-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
 	  //modulos: ruteo, formularios, peticiones
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	FormsModule
   ],
   providers: [
 	  //servicios: fuentes de datos
