c4e317c1d9e23a3fc7eed06859687e43eb658e7e
Jorge Montesinos <jmontesinos@conflux-aqp.com>
2021-11-26 22:09:50 -0500

    update

--- Semana13/my-project/src/app/components/productos/productos.component.ts
+++ Semana13/my-project/src/app/components/productos/productos.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductosService } from '../../services/productos.service';
+import { Subscription } from 'rxjs'; //tipos, types
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.css']
+  styleUrls: ['./productos.component.css'],
 })
 export class ProductosComponent implements OnInit {
+  misProductos: Array<any>;
+  subscripcionProdServ: Subscription;
 
-  constructor() { }
+  constructor(private _sProductos: ProductosService) {}
 
-  ngOnInit(): void {
+  getProductos() {
+    this.subscripcionProdServ = this._sProductos
+      .obtenerProductos()
+      .subscribe((datos) => {
+        console.log(datos);
+      });
   }
 
+  ngOnInit(): void {
+    //equivale a un useEffect justo después del montaje
+    this.getProductos();
+  }
 }
