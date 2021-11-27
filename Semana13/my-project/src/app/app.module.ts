41fa6ca258b121f110e6609797e6cd2bc5800209
Jorge Montesinos <jmontesinos@conflux-aqp.com>
2021-11-26 21:39:04 -0500

    update

--- Semana13/my-project/src/app/app.module.ts
+++ Semana13/my-project/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+	HttpClientModule
+	
   ],
   providers: [],
   bootstrap: [AppComponent]
