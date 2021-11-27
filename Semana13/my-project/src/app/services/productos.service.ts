98db53f6beba488e3789f011c7bf8224c6ba19dc
Jorge Montesinos <jmontesinos@conflux-aqp.com>
2021-11-26 21:55:54 -0500

    update

--- Semana13/my-project/src/app/services/productos.service.ts
+++ Semana13/my-project/src/app/services/productos.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+//Es la herramienta que me permite hacer peticiones en angular
+import { HttpClient } from '@angular/common/http';
+//Observable es la manera que se va a enviar el resultado de mis peticiones, type
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductosService {
+  url: string = 'https://616b5ead16c3fa001717167c.mockapi.io/productos';
 
-  constructor() { }
+  //   inyección por dependencias, declarando como una propiedad privada más
+  //const _Http =  new HttpClient()
+  constructor(private _Http: HttpClient) {}
+
+  obtenerProductos(): Observable<any> {
+    return this._Http.get(this.url);
+  }
 }
